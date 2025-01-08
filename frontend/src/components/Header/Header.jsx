e12d149f1b9ae02b593d5c0471cda53110b10315
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-01-08 19:09:41 +0530

    initial folder

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>Step into your cozy <span className='nomnom'>NOM-NOM</span> Era</h2>
+            <p>Craving something tasty? Explore a variety of cuisines and enjoy your favorite meals delivered fresh and fast, straight to your doorstep. Satisfy your hunger with just a few clicks!</p>
+            <button>View Menu</button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
