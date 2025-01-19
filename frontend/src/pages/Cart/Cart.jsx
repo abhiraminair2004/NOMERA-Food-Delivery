aba8de5f86160a5521aea441d71306d9d3db7226
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-01-20 01:47:15 +0530

    started place order

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,8 +1,10 @@
 import React, { useContext } from "react";
 import "./Cart.css";
+import {useNavigate} from 'react-router-dom';
 import { StoreContext } from "../../context/StoreContext";
 const Cart = () => {
   const { cartItems, food_list, removeFromCart,getTotalCartAmount } = useContext(StoreContext);
+  const navigate= useNavigate();
   return (
     <div className="cart">
       <div className="cart-items">
@@ -53,7 +55,7 @@ const Cart = () => {
               <b>${getTotalCartAmount()+2}</b>
             </div>
           </div>
-          <button>PROCEED TO CHECKOUT</button>
+          <button onClick={()=>navigate('/order')}>PROCEED TO CHECKOUT</button>
         </div>
         <div className="cart-promocode">
           <p>If you have a promo code, Enter it here</p>
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 import './PlaceOrder.css'
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name'/>
+          <input type="text" placeholder='Last Name'/>
+        </div>
+        <input type="text" placeholder='Email address'/>
+        <input type="text" placeholder='Street'/>
+        <div className="multi-fields">
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State'/>
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code'/>
+          <input type="text" placeholder='Country'/>
+        </div>
+        <input type="text" placeholder='Phone '/>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
   )
 }
 
