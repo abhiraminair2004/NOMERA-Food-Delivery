9a824090a8038bf078d78b2d58892f3bb41b70ab
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-01-20 01:05:24 +0530

    food amount calculated

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -25,16 +25,24 @@ const StoreContextProvider = (props) => {
         });
     };
 
-    // Log cartItems whenever it changes
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems]);
+    const getTotalCartAmount=()=>{
+        let totalAmount=0;
+        for(const item in cartItems)  {
+            if(cartItems[item]>0){
+                let itemInfo= food_list.find((product)=>product._id===item)
+                totalAmount+= itemInfo.price*cartItems[item];
+            }
+            
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
         cartItems,
         addToCart,
         removeFromCart,
+        getTotalCartAmount
     };
 
     return (
