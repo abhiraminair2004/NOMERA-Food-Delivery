284452b07f05adaad285134857a2bb31bb83044d
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-01-29 00:39:35 +0530

    backend api set for admin panel

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -3,6 +3,7 @@ import fs from "fs";
 //add food item
 
 const addFood = async (req, res) => {
+
   let image_filename = `${req.file.filename}`;
   const food = new foodModel({
     name: req.body.name,
@@ -20,4 +21,26 @@ const addFood = async (req, res) => {
     res.json({success:false,message:"Error"})
   }
 };
-export { addFood };
+//all food list
+const listFood= async(req,res)=>{
+  try {
+    const foods= await foodModel.find({});
+    res.json({success:true,data:foods})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
+//remove food item
+const removeFood=async(req,res)=>{
+  try {
+    const food=await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`,()=>{})
+    await foodModel.findByIdAndDelete(req.body.id)
+    res.json({success:true,message:"Food Removed"})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+}
+export { addFood, listFood, removeFood};
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Fix __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 //app config
 const app= express()
@@ -15,6 +22,8 @@ app.use(cors())
 connectDB();
 //api endpoint
 app.use("/api/food",foodRouter)
+app.use("/images", express.static(path.join(__dirname, "uploads")));
+
 
 app.get("/",(req,res)=>{
     res.send("API working")
