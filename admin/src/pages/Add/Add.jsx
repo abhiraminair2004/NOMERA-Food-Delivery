cdec61e36a84fa97b3c96e160dd57975e8601ad5
Abhirami Nair <abhiraminairmalu@gmail.com>
2025-02-11 15:12:52 +0530

    toastify

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,29 +1,87 @@
-import React from 'react'
-import './Add.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from "react";
+import "./Add.css";
+import axios from "axios";
+import { assets } from "../../assets/assets";
+import { toast } from "react-toastify";
 const Add = () => {
+  const url = "http://localhost:4000";
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((data) => ({ ...data, [name]: value }));
+  };
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false)
+      toast.success(response.data.message)
+    } else {
+      toast.error(response.data.message)
+    }
+  };
   return (
     <div className="add">
-      <form className='flex-col'>
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image" hidden/>
+          <input
+            onChange={(event) => setImage(event.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product name</p>
-          <input type='text' name='name' placeholder='Type here'/>
+          <input
+            onChange={onChangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
         </div>
         <div className="add-product-description flex-col">
           <p>Product description</p>
-          <textarea name="description" rows="6" placeholder='write content here' required></textarea>
+          <textarea
+            onChange={onChangeHandler}
+            value={data.description}
+            name="description"
+            rows="6"
+            placeholder="write content here"
+            required
+          ></textarea>
         </div>
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product category</p>
-            <select name="category">
+            <select onChange={onChangeHandler} name="category">
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -36,13 +94,21 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Product Price</p>
-            <input type="Number" name="price" placeholder='$20'/>
+            <input
+              onChange={onChangeHandler}
+              value={data.price}
+              type="Number"
+              name="price"
+              placeholder="$20"
+            />
           </div>
         </div>
-        <button type='submit' className='add-btn'>ADD</button>
+        <button type="submit" className="add-btn">
+          ADD
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Add
+export default Add;
